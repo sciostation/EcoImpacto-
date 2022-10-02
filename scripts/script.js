764bdbd71d8bdd23b93d9744a9500c74b19032db
Bianca La Manna <92586945+Bianca-LM@users.noreply.github.com>
2022-10-02 12:47:42 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -114,7 +114,7 @@ function load(url) {
 		success: function(data) {
 			newArticle = $('#article').html(data);
 			if (window.location.pathname.includes("documentation.html") || window.location.pathname.includes("about.html")) {
-				sessionStorage.setItem("article", JSON.stringify(newArticle);
+				sessionStorage.setItem("article", JSON.stringify(newArticle));
 				window.location.href = "https://bianca-lm.github.io/coolDown/";
 			}
 			else {
